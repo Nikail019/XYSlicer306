c7cf3dcbfd24093b2a025457c03b314ba7f48b4a
SlashApollo <wilkendevon@gmail.com>
2025-08-01 16:04:04 +1200

    Friday 1st august

---
+++ src/Baremetal_motor_controll_1.cpp
@@ -0,0 +1,35 @@
+#include <avr/io.h>
+
+int main(void) {
+    // Set pin 4 (PG5) as output
+    DDRG |= (1 << PG5);
+    // Set pin 5 (PE3) as output
+    DDRE |= (1 << PE3);
+
+    // Set pin 4 HIGH
+    PORTG |= (1 << PG5);// Motor forward direction
+    // Set pin 5 HIGH
+
+    //timer 2 setup
+    TCCR2A |= (1<<COM2A0); //toggle on compare match
+    //wgm2 for ctc with top ocra
+    
+    // Set CTC mode (WGM2[2:0] = 010)
+    TCCR2A = (1 << WGM21);
+    TCCR2B = (1 << CS22);  // Prescaler 64
+
+    OCR2A = 249; // 1 ms interval with 16MHz clock and prescaler 64
+
+    TIMSK2 = (1 << OCIE2A); // Enable compare match A interrupt
+
+
+
+
+    while (1) {
+    }
+
+}
+    ISR(TIMER2_COMPA_vect) {
+    // Called every 1 ms
+    
+}
--- src/main.cpp
+++ src/main.cpp
@@ -3,8 +3,12 @@
 #include <util/delay.h>
 #include <avr/interrupt.h>
 
-bool x_debounce_flag = 0;
-bool y_debounce_flag = false;
+volatile bool x_debounce_flag = 0;
+volatile bool y_debounce_flag = false;
+volatile bool hit_top = false;
+volatile bool hit_right = false;
+volatile bool hit_left = false;
+volatile bool hit_bottom = false;
 
 
 
@@ -26,13 +30,32 @@ int main(){
   TIMSK1 |= (1 << OCIE1A); //ISR on Compare Match
   TIMSK3 |= (1 << OCIE3A); 
 
-
-  //setup int0:4 pins for input
-  DDRD &= ~((1 << PD0 ) | (1 << PD1) | (1 << PD2) | (1 << PD3));
-
-  //enable interrup on those pins on rising edge
-  EIMSK |= (1 << INT0) | (1 << INT1) | (1 << INT2) | (1 << INT3);
-  EICRA |= 0xFF; //ALL ARE HIGH FOR RISING EDGE 
+  // // LIMIT SWITCH SETUP
+  // //  TOP, RIGHT, LEFT, BOTTOM == D21, D20, D19, 18
+  // DDRD &= ~((1 << PD0 ) | (1 << PD1) | (1 << PD2) | (1 << PD3));
+  // //enable interrup on those pins on rising edge
+  // EIMSK |= (1 << INT0) | (1 << INT1) | (1 << INT2) | (1 << INT3);
+  // EICRA |= 0xFF; //ALL ARE HIGH FOR RISING EDGE 
+  
+  // LIMIT SWITCH INTERRUPT SETUP
+  //  TOP, RIGHT, LEFT, BOTTOM == D21, D20, D19, D18
+  attachInterrupt(digitalPinToInterrupt(21), LimitTop , RISING);
+  attachInterrupt(digitalPinToInterrupt(20), LimitRight , RISING);
+  attachInterrupt(digitalPinToInterrupt(19), LimitLeft, RISING);
+  attachInterrupt(digitalPinToInterrupt(18), LimitBottom, RISING);
+  
+  // ENCODER INTERRUPT SETUP
+  // LEFT MOTOR (A1, B1), RIGHT MOTOR (A2, B2)
+  attatchInterrupt(digitalPinToInterrupt(2), EncoderA1, CHANGE);
+  attachInterrupt(digitalPinToInterrupt(3), EncoderA2, CHANGE);
+  
+ // Pin Change Interrupts
+  //B1 -> D15, PJO, PCINT[10], B2 -> D69/A15, PK7, PCINT[23]
+  // B1 uses bit 1, B2 uses bit 2
+  PCICR |= (1 << PCIE1) | (1 << PCIE2);
+  PCMSK1 |= ( 1 << PCINT1O); 
+  PCMSK2 |= ( 1 << PCINT23);
+  
   sei();
 
   while(1){
@@ -40,41 +63,85 @@ int main(){
   }
 }
 
-//x direction limit switches (Timer 1)
-ISR(INT0_vect){
-  //Reset timer 1
+
+ISR(PCINT1_vect){
+ // Interrupt for Encoder channel B1 
+}
+
+ISR(PCINT2_vect){
+  // Interrupt for Encoder channel B2
+}
+
+void LimitTop(){
   if (x_debounce_flag == 0){
     TCNT1 = 0;
     //method
+    hit_top = true;
     x_debounce_flag = true;
   }
-
 }
 
-ISR(INT1_vect){
+void LimitRight(){
   if (x_debounce_flag == 0){
     TCNT1 = 0;
     //method
+    hit_right = true;
     x_debounce_flag = true;
   }
 }
-// y direction limit switches (timer 3)
-ISR(INT2_vect){
-    if (y_debounce_flag == 0){
+
+void LimitLeft(){
+  if (y_debounce_flag == 0){
     TCNT3 = 0;
     //method
-    x_debounce_flag = true;
+    hit_left = true;
+    y_debounce_flag = true;
   }
 }
 
-ISR(INT3_vect){
+void LimitBottom(){
   if (y_debounce_flag == 0){
     TCNT3 = 0;
     //method
     y_debounce_flag = true;
   }
-
 }
+
+// //x direction limit switches (Timer 1)
+// ISR(INT0_vect){
+//   //Reset timer 1
+//   if (x_debounce_flag == 0){
+//     TCNT1 = 0;
+//     //method
+//     x_debounce_flag = true;
+//   }
+
+// }
+
+// ISR(INT1_vect){
+//   if (x_debounce_flag == 0){
+//     TCNT1 = 0;
+//     //method
+//     x_debounce_flag = true;
+//   }
+// }
+// // y direction limit switches (timer 3)
+// ISR(INT2_vect){
+//     if (y_debounce_flag == 0){
+//     TCNT3 = 0;
+//     //method
+//     x_debounce_flag = true;
+//   }
+// }
+
+// ISR(INT3_vect){
+//   if (y_debounce_flag == 0){
+//     TCNT3 = 0;
+//     //method
+//     y_debounce_flag = true;
+//   }
+
+// }
 
 ISR(TIMER1_COMPA_vect){
   x_debounce_flag = false;
